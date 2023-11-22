82927172d60a42d32432f487e657a1c01a475b39
Bishal Chettri <bishalin01@gmail.com>
2023-11-22 11:56:11 +0530

    Testing for checkout

---
+++ unit-testing-TDD/CheckoutKata/checkout.py
@@ -0,0 +1,55 @@
+class Checkout:
+    class Discount:
+        def __init__(self, no_of_items, price):
+            self.nbr_items = no_of_items
+            self.price = price
+
+    def __init__(self):
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
+
+    def add_discount(self, item, no_of_items, price):
+        discount = self.Discount(no_of_items, price)
+        self.discounts[item] = discount
+
+    def add_item_price(self, item, price):
+        self.prices[item] = price
+
+    def add_item(self, item):
+        if item not in self.prices:
+            raise Exception("Bad item")
+
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
+    def calculateTotal(self):
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.calculateItemTotal(item, cnt)
+
+        return total
+
+    def calculateItemTotal(self, item, cnt):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if cnt >= discount.nbr_items:
+                total += self.calculate_item_discount_total(item, cnt, discount)
+            else:
+                total += self.prices[item] * cnt
+        else:
+            total += self.prices[item] * cnt
+
+        return total
+
+    def calculate_item_discount_total(self, item, cnt, discount):
+        total = 0
+        no_of_discounts = cnt / discount.nbr_items
+        total += no_of_discounts * discount.price
+        remaining = cnt % discount.nbr_items
+        total += remaining * self.prices[item]
+
+        return total
